f67c4a31c6e73d72e471270a708a49cfd5629575
Tim Officer <timothyofficer@gmail.com>
2022-03-01 14:14:53 +0000

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/menu">Menu</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
